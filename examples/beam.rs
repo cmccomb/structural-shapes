e6e59cbff9695aa1381bf6a425388338b215df37
Chris McComb <ccmcc2012@gmail.com>
2023-02-18 18:19:20 -0500

    Beam calculation example

---
+++ examples/beam.rs
@@ -0,0 +1,20 @@
+use structural_shapes::{meters, StructuralShape};
+use uom::si::f64::Torque;
+use uom::si::torque::newton_meter;
+
+fn main() {
+    // Define height of cross-section
+    let y = meters(0.25);
+
+    // Define the shape of the cross-section
+    let I = StructuralShape::new_ibeam(0.5, 0.25, 0.025, 0.05).area();
+
+    // Define the moment resisted by the cross-section
+    let M = Torque::new::<newton_meter>(10000.0);
+
+    // Define stress
+    let stress = M * y / I;
+
+    // Compute and print stress
+    println!("{:?}", stress)
+}
