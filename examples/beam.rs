46605528dfb65798aba32ef5d02864166d1ac719
Chris McComb <ccmcc2012@gmail.com>
2023-02-19 09:02:50 -0500

    Beam example now prints nicely

--- examples/beam.rs
+++ examples/beam.rs
@@ -1,5 +1,7 @@
 use structural_shapes::{meters, StructuralShape};
-use uom::si::f64::Torque;
+use uom::fmt::DisplayStyle;
+use uom::si::f64::{Pressure, Torque};
+use uom::si::pressure::{megapascal, pascal};
 use uom::si::torque::newton_meter;
 
 fn main() {
@@ -7,14 +9,14 @@ fn main() {
     let y = meters(0.25);
 
     // Define the shape of the cross-section
-    let I = StructuralShape::new_ibeam(0.5, 0.25, 0.025, 0.05).area();
+    let I = StructuralShape::new_ibeam(0.5, 0.25, 0.025, 0.05).moi_x();
 
     // Define the moment resisted by the cross-section
     let M = Torque::new::<newton_meter>(10000.0);
 
-    // Define stress
-    let stress = M * y / I;
+    // Make a formatter to use with MPa
+    let mpa = Pressure::format_args(megapascal, DisplayStyle::Abbreviation);
 
     // Compute and print stress
-    println!("{:?}", stress)
+    println!("{}", mpa.with(M * y / I));
 }
